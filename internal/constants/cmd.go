0ba8e2fd96180e8ad01cc56c5b56475f423554e5
lpmatos <luccapessoamatos@gmail.com>
2020-09-28 08:32:04 -0300

    feat: adding new functions

--- internal/constants/cmd.go
+++ internal/constants/cmd.go
@@ -1,5 +1,14 @@
 package constants
 
+// RootHelpMessage - return the long description of root command.
+const RootHelpMessage = `Description:
+
+A CLI tool that automate your project startup:
+
+- Template project with pretty README.md, LICENSE, gitignore
+- Good Commit Messages Pattern with Conventional Commits
+`
+
 // CompletionHelpMessage - return the long description of completion command.
 const CompletionHelpMessage = `To load completions:
 
---
+++ internal/constants/default.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	symbolInfo    = "• " // symbolInfo is a const that represent a info symbol
+	symbolSuccess = "🗸 " // symbolInfo is a const that represent a success symbol
+	symbolWarning = "⚡"  // symbolInfo is a const that represent a warning symbol
+	symbolError   = "✗ " // symbolInfo is a const that represent a error symbol
+)
+
+// ReadmeSuccessfulyMessage - return the readme successfully message.
+const ReadmeSuccessfulyMessage = `
+╔═════════════════════════════════════════════╗
+║                                             ║
+║  README.md was successfully generated.      ║
+║ Thanks for using Infinity Deploy Generator! ║
+║                                             ║
+╚═════════════════════════════════════════════╝
+`
