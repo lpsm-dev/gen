d2fde2e710bee583a5d7fdee185bf6c731f853dd
lpmatos <luccapessoamatos@gmail.com>
2020-09-29 03:05:54 -0300

    fix: makefile and compile go releaser flags

--- internal/version/version.go
+++ internal/version/version.go
@@ -11,16 +11,18 @@ import (
 // These variables are populated via the Go ldflags.
 // This will be filled in by the compiler.
 var (
-	_UTCBuildTime  = "unknown-utc-build-time"
-	_ClientVersion = "unknown-cli-version"
-	_GoVersion     = "unknown-go-version"
-	_GitBranch     = "unknown-git-branch"
-	_GitHash       = "unknown-git-hash"
+	cliVersion   = "unknown-cli-version"
+	builtDate    = "unknown-built-date"
+	builtBy      = "unknown-built-by"
+	commit       = "unknown-commit"
+	commitShort  = "unknown-short-commit"
+	commitBranch = "unknown-commit-branch"
+	goVersion    = "unknown-go-version"
 )
 
 // GetDisplay function - parse current version and return a formatted string.
 func GetDisplay() string {
-	return fmt.Sprintf("CLI Version - %s", _ClientVersion)
+	return fmt.Sprintf("CLI Version - %s", cliVersion)
 }
 
 // GetPrettyDetails function - create a pretty table and parse this table with current version details.
@@ -29,10 +31,11 @@ func GetPrettyDetails() {
 	versionTable.SetOutputMirror(os.Stdout)
 	versionTable.AppendHeader(table.Row{"Info", "Content"})
 	versionTable.AppendRows([]table.Row{
-		{"UTC Build Time", _UTCBuildTime},
-		{"Go Version", _GoVersion},
-		{"Git Branch", _GitBranch},
-		{"Git Hash", _GitHash},
+		{"Build Date", builtDate},
+		{"Build by", builtBy},
+		{"Commit Short", commitShort},
+		{"Commit Branch", commitBranch},
+		{"Go Version", goVersion},
 	})
 	versionTable.SetStyle(table.StyleColoredBright)
 	versionTable.Render()
